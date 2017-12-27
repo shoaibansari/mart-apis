a8c89d9f7442a0d2f3d0b4fc9a1f83680ee6b9b8
shoaibansari <shoaibansari824@gmail.com>
2017-12-27 20:58:56 +0500

    hosting

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'www.extralicio.us'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'armancom_appdb'),
             'username' => env('DB_USERNAME', 'armancom_appuser'),
